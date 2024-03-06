a0b29604076f2faeb17aff05c634cbac7cd72232
Sarah Omoike <Sarah.Omoike@PremiumTrustBank.com>
2024-03-06 15:40:13 +0100

    Added a few validations

---
+++ ApplicationServices/Validations/TSAReportValidation.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationServices.Validations
+{
+   
+    public class TSAReportValidation : AbstractValidator<TSAReport>
+    {
+        public TSAReportValidation()
+        {
+            ClassLevelCascadeMode = CascadeMode.Stop;
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x.checkerConfig.InitiatorBranch)
+                .NotEmpty()
+                .WithMessage("{InitiatorBranch} is required.");
+
+            RuleFor(x => x.checkerConfig.StatusCode)
+                .NotEmpty()
+                .WithMessage("{StatusCode} is required.");
+     
+
+            RuleFor(x => x.Channel)
+               .NotEmpty()
+               .WithMessage("{Channel} is required.")
+               .MaximumLength(10)
+               .WithMessage("{Channel} has minimum range.");
+        }
+    }
+}
